fecbe1edccba1680deddd2a0987e77c390ff145f
juhus <soh.ju.hu@gmail.com>
2012-05-24 22:10:44 +0800

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,10 +12,10 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
+  has_many :microposts, dependent: :destroy  
 
   before_save { self.email.downcase! }
   before_save :create_remember_token
-
   
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -24,6 +24,11 @@ class User < ActiveRecord::Base
   			uniqueness: { case_sensitive: true}
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
 
 private
   def create_remember_token
--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -1,4 +1,4 @@
-	namespace :db do
+namespace :db do
   desc "Fill database with sample data"
   task populate: :environment do
     admin = User.create!(name: "Example User",
@@ -15,6 +15,12 @@
                    email: email,
                    password: password,
                    password_confirmation: password)
+    end
+      
+    users = User.all(limit: 6)
+    50.times do
+        content = Faker::Lorem.sentence(5)
+        users.each { |user| user.microposts.create!(content: content) }
     end
   end
 end
